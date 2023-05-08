b900786849a87f40fc17e0b56e20cb9f490af087
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-08 07:00:53 -0700

    Linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list has a loop
+ * @list: pointer to head of list
+ *
+ * Return: 0 || 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	if (list == NULL)
+		return (1);
+
+	fast = list->next;
+	slow = list;
+
+	while (fast && fast->next && slow)
+	{
+		if (fast == slow)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	return (0);
+}
