c0008e3c2e1ca465779f20d2e6a8f0990a8eafdb
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-09 02:19:45 -0700

    Print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+
+def print_last_digit(number):
+    absval = number * -1 if number < 0 else number
+    unsigned_number = absval % 10
+    print(unsigned_number, end="")
+    return unsigned_number
