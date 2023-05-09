75355854b264cfffb151bf287d522be38d2c9c68
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-09 04:28:40 -0700

    Hot Fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{d}{e}".format(d=i,e=j), end=", " if i < 8 else "\n")
+        print("{d}{e}".format(d=i, e=j), end=", " if i < 8 else "\n")
