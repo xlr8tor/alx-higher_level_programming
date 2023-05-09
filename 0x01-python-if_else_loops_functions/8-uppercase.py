0e6456cae432db2e6e35d185e56885d168b56937
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-09 04:12:36 -0700

    Add shabang

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uppercase(str):
     newstr = ""
     for c in str:
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 
 def print_last_digit(number):
     absval = number * -1 if number < 0 else number
