644cd89a2c175f1412003a46589fa39a0ab01838
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-09 02:37:57 -0700

    To uppercase:

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+def uppercase(str):
+    newstr = ""
+    for c in str:
+        c = ord(c)
+
+        if c > 96 and c < 123:
+            newstr += chr(c - 32)
+        else:
+            newstr += chr(c)
+    print(newstr)
