0f214df60f0701d7c050130a0add015187dc943a
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-11 06:29:10 -0700

    Infinite Add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys
 
-total = 0
-for num in sys.argv[1:]:
-    total += int(num)
-print(f"{total:d}")
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+    for num in sys.argv[1:]:
+        total += int(num)
+    print(f"{total:d}")
