be09bb0a927c7119c6cb194d83d4617a876a5892
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-11 06:26:37 -0700

    2 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import sys
 
-length = len(sys.argv) - 1
-msg = "arguments" if (length != 1) else "argument"
-msg += "." if (length == 0) else ":"
+if __name__ == "__main__":
+    import sys
 
-print("{} {}".format(length, msg))
-for index, arg in enumerate(sys.argv[1:]):
-    print("{}: {}".format(index + 1, arg))
+    length = len(sys.argv) - 1
+    msg = "arguments" if (length != 1) else "argument"
+    msg += "." if (length == 0) else ":"
+
+    print("{} {}".format(length, msg))
+    for index, arg in enumerate(sys.argv[1:]):
+        print("{}: {}".format(index + 1, arg))
