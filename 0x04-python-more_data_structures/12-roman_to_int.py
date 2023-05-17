05384621820548e5e0c02e76a17f5b07c8edc7fd
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-17 03:39:05 -0700

    Tasks 0 - 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    numeral = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    number = numeral[roman_string[len(roman_string) - 1]]
+    for i in range(len(roman_string) - 1, 0, -1):
+        prev = numeral[roman_string[i - 1]]
+        curr = numeral[roman_string[i]]
+
+        if prev >= curr:
+            number += prev
+        else:
+            number -= prev
+    return number
