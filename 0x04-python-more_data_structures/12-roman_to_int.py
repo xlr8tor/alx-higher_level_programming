f43adff9f85d267b0a1871d873cbf53e15a14754
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-17 04:08:15 -0700

    Fix pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or type(roman_string) is not str:
         return 0
     numeral = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     number = numeral[roman_string[len(roman_string) - 1]]
