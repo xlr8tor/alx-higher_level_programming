68fac5f3da64831eff2c1da1c6d0a1003c01588b
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-29 04:06:06 -0700

    Fix pycodestyle

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 
 def safe_print_division(a, b):
     try:
-        result =  a/b
+        result = a/b
     except ZeroDivisionError:
-        result =  None
+        result = None
     finally:
         print("Inside result: {}".format(result))
     return result
