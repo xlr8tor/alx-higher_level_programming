b0635f7d95c55e2891ac683c6e342aa7b89a52c0
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-30 06:48:06 -0700

    Fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__position):
+            for i in range(self.__position[1]):
                 print("")
             for i in range(self.size):
                 print(" " * self.__position[0], end="")
