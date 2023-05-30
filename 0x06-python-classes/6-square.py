6a61fed1e5aac80ba4967bdda1f836e62ff5a6e9
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-30 04:47:34 -0700

    Add task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """square class with size and proper validation"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            line = "#" * self.size
+            for i in range(self.size):
+                print(" " * self.__position[0])
+                print(line)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and len(value) != 2:
+            if value[0] < 0 or value[1] < 0:
+                raise TypeError("position must be a tuple \
+                        of 2 positive integers")
+        else:
+            self.__position = value
