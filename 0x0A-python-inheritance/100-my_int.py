3f636d4b6844f9b4919ad641af164cbcb8f7645c
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-06-12 06:05:28 -0700

    100 2

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-"""Defines a class MyInt that inherits from int."""
+"""Defines a class MyInt that inherits from int"""
 
 
 class MyInt(int):
     """MyInt has == and != operators inverted"""
-    def __eq__(self, other):
+
+    def __eq__(self, value):
         """switch == with !="""
         return !(self.real == other)
 
-    def __ne__(self, other):
+    def __ne__(self, value):
         """switch != with =="""
         return !(self.real != other)
