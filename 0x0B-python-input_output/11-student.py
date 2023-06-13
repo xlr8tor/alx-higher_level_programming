e68dab95b0971720547a45abd87c00c2c6e1e17e
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-06-13 09:29:47 -0700

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""My module"""
+
+
+class Student:
+    """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Creates a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Student Dictionary"""
+        if attrs is None:
+            return self.__dict__
+        ndict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                ndict[key] = value
+        return ndict
+
+    def reload_from_json(self, json):
+        """Relaod from json"""
+        for k, v in json.items():
+            setattr(self, k, v)
