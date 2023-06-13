7459bd72ba3117f4657226ffc1aa33da372bd482
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-06-13 09:08:33 -0700

    Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""My module"""
+
+
+class Student:
+    """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Creates a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Student Dictionary"""
+        return self.__dict__
